6d092aec4b0eadf3f5ff392d74346c30473bd2b0
sriramrudraraju <sriram.rudraraju6@gmail.com>
2020-04-06 13:38:23 -0500

    feat: sorting dnd

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,40 +1,44 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback  } from 'react';
 
-import { Board } from '../board/board';
+import { Sorting } from '../sorting/sorting.component';
 
 import './app.css';
 
-const START_POSITION: [number, number] = [0, 0];
-
-export function canMoveKnight(toX: number, toY: number, fromPosition: [number, number]) {
-  const [x, y] = fromPosition;
-  const dx = toX - x
-  const dy = toY - y
-
-  return (
-    (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
-    (Math.abs(dx) === 1 && Math.abs(dy) === 2)
-  )
-}
+const LIST = [
+  {
+    id: 1,
+    children: '1',
+  },
+  {
+    id: 2,
+    children: '2',
+  },
+  {
+    id: 3,
+    children: '3',
+  },
+  {
+    id: 4,
+    children: '4',
+  }
+];
 
 function App() {
-  const [knightPosition, setKnightPosition] = useState(START_POSITION);
 
-  const moveTo = useCallback(
-    (posiiton: [number, number]) => {
-      const canKnightMv = canMoveKnight(posiiton[0], posiiton[1], knightPosition);
-      if (canKnightMv) {
-        setKnightPosition(posiiton);
-      }
+  const [list, setList] = useState(LIST);
+  const onListChange = useCallback(
+    (eleList) => {
+      setList(eleList);
     },
-    [knightPosition]
+    [setList]
   )
+
   return (
     <div className="app-root">
-      <Board 
-        style={{height: 600, width: 600, margin: 'auto'}} 
-        knightPosition={knightPosition} 
-        moveTo={moveTo} 
+      <Sorting 
+        list={list}
+        onListChange={onListChange}
+        columns={3}
       />
     </div>
   );
---
+++ src/components/sorting/sorting.component.tsx
@@ -0,0 +1,62 @@
+import React, { FC, useCallback } from 'react';
+
+import { Cell } from './cell/cell';
+
+import { shiftItemFromTo } from './utils/shift-item-from-to';
+
+interface List {
+  id: number | string;
+  children: string | JSX.Element | null;
+}
+
+interface SortingProps {
+  list: List[]; // array of elements
+  onListChange: (list: List[]) => void;
+  columns?: number; // number of elements per row; default = 1
+  style?: object;
+}
+export const Sorting: FC<SortingProps> = ({ list, columns = 1, onListChange, style }) => {
+
+  const findCell = useCallback(
+    (id: string) => {
+      const cell = list.filter((c) => `${c.id}` === id)[0]
+      return {
+        cell,
+        index: list.indexOf(cell),
+      }
+    },
+    [list]
+  )
+
+  const moveCell = useCallback(
+    (dragIndex: string, hoverIndex: number) => {
+      const { index } = findCell(dragIndex);
+      const newList = shiftItemFromTo(index, hoverIndex, list);
+      onListChange(newList);
+    },
+    [list, onListChange, findCell]
+  );
+
+  return (
+    <div 
+      style={{
+        display: 'flex', 
+        flexWrap: 'wrap', 
+        justifyContent: 'space-between',
+        ...style
+      }}
+    >
+      {list.map((ele) => (
+        <Cell
+          key={ele.id}
+          id={`${ele.id}`}
+          moveCell={moveCell}
+          findCell={findCell}
+          columns={columns}
+        >
+          {ele.children}
+        </Cell>
+      ))}
+    </div>
+  )
+}
