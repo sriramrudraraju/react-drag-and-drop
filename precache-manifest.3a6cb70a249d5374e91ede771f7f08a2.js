239ee8f879a08f04343c12029da5dbed2b00a048
sriramrudraraju <sriram.rudraraju6@gmail.com>
2020-04-19 23:59:49 +0000

    Deploying to gh-pages from master e545a164be356d5e5cd6a8cb4f3146f1cb0e8ce2

---
+++ precache-manifest.3a6cb70a249d5374e91ede771f7f08a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfdd96b5bacb3c86ce4cf91a28c6d60c",
+    "url": "/react-drag-and-drop/index.html"
+  },
+  {
+    "revision": "dc4c640cf4c9165a42de",
+    "url": "/react-drag-and-drop/static/css/main.9bfc0a1c.chunk.css"
+  },
+  {
+    "revision": "5d83f8603aa23593727e",
+    "url": "/react-drag-and-drop/static/js/2.0dfdeeeb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-drag-and-drop/static/js/2.0dfdeeeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc4c640cf4c9165a42de",
+    "url": "/react-drag-and-drop/static/js/main.9dacb8ef.chunk.js"
+  },
+  {
+    "revision": "acb0903b884f916a9c29",
+    "url": "/react-drag-and-drop/static/js/runtime-main.d5c1f2f3.js"
+  }
+]);
